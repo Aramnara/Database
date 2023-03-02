c992c1de43d2915c964d5ea53724a0e3d09f8f52
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-03-02 17:10:05 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,12 +1,17 @@
-(SELECT 'SHIPPED' AS ship_status, order_id, order_date
-FROM Orders
-
-Where ship_date IS NOT NULL)
-UNION
-
-(SELECT 'NOT SHIPPED' AS ship_status, order_id, order_date
-FROM Orders
-
-Where ship_date IS NULL)
-
+SELECT 
+    'SHIPPED' AS 'Ship Status',
+    order_id AS 'The order id',
+    order_date
+FROM
+    orders
+WHERE
+    ship_date IS NOT NULL 
+UNION SELECT 
+    'NOT SHIPPED' AS 'Ship Status',
+    order_id AS 'The order id',
+    order_date
+FROM
+    orders
+WHERE
+    ship_date IS NULL
 ORDER BY order_date;
