419a2edbebf216e8de12f3e3b62d6af5b373f228
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-03-02 16:57:02 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,6 @@
-SELECT c.category_name, p.product_name, p.list_price
-FROM categories c
-JOIN products p
-ON c.category_id=p.category_id
-ORDER BY c.category_name, p.product_name
+SELECT c.first_name, c.last_name, a.line1, a.city, a.state, a.zip_code
+FROM customers c
+JOIN addresses a
+ON c.customer_id=a.customer_id
+WHERE c.email_address = 'allan.sherwood@yahoo.com'
+ORDER BY a.zip_code
