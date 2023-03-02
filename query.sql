e9da1856d18aafa2dabdb4e558ab9429fb46711b
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-03-02 16:42:38 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,7 @@
-SELECT product_name, list_price, discount_percent,
-ROUND(list_price*discount_percent/100, 2) AS 'discount_amount',
-list_price-'discount_amount' AS 'discount_price'
-FROM products
-ORDER BY discount_price DESC
-LIMIT 5
+SELECT item_id, item_price,discount_amount, quantity,
+quantity*item_price AS price_total,
+discount_amount*quantity AS discount_total,
+(item_price-discount_amount)*quantity AS item_total
+FROM order_items
+WHERE (item_price-discount_amount)*quantity > 500
+ORDER BY item_total DESC
