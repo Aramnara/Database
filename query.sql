6abf9815f0511242b729a8b1cc7d21ea9cef58f4
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-03-02 16:58:05 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
 SELECT c.first_name, c.last_name, a.line1, a.city, a.state, a.zip_code
 FROM customers c
 JOIN addresses a
-ON c.customer_id=a.customer_id
-WHERE c.email_address = 'allan.sherwood@yahoo.com'
-ORDER BY a.zip_code
+ON c.customer_id = a.customer_id
+AND c.shipping_address_id = a.address_id
+ORDER BY zip_code
