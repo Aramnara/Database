e8244dd8af6736ab7935fbef01c53bfaa8e5e0c6
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-03-02 17:15:23 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,17 +1,4 @@
-SELECT 
-    'SHIPPED' AS 'Ship Status',
-    order_id AS 'The order id',
-    order_date
-FROM
-    orders
-WHERE
-    ship_date IS NOT NULL 
-UNION SELECT 
-    'NOT SHIPPED' AS 'Ship Status',
-    order_id AS 'The order id',
-    order_date
-FROM
-    orders
-WHERE
-    ship_date IS NULL
-ORDER BY order_date;
+insert into categories 
+(category_id, category_name)
+values 
+(default, 'Brass');
