4842e619abe7d0208ca3f69de139aa2a5accd76d
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 16:02:26 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,17 @@
-SELECT order_id, order_date, ship_date
-FROM orders
-WHERE ship_date IS NULL
-ORDER BY order_id DESC;
+SELECT 
+  c.last_name, 
+  c.first_name, 
+  o.order_date, 
+  p.product_name, 
+  oi.list_price AS item_price, 
+  ROUND(oi.list_price * oi.discount_percent / 100, 2) AS discount_amount, 
+  oi.quantity 
+FROM 
+  Customers c 
+  JOIN Orders o ON c.customer_id = o.customer_id 
+  JOIN Order_Items oi ON o.order_id = oi.order_id 
+  JOIN Products p ON oi.product_id = p.product_id 
+ORDER BY 
+  c.last_name, 
+  o.order_date, 
+  p.product_name;
