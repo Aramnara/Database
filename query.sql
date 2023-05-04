8effdb158261d092e6fe5cc044a90885d8a589f4
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 17:48:23 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,12 +1,6 @@
-SELECT 
-  c.email_address, 
-  SUM(oi.list_price * oi.quantity) AS item_price_total, 
-  SUM((oi.list_price * oi.discount_percent / 100) * oi.quantity) AS discount_amount_total 
-FROM 
-  customers c 
-  JOIN orders o ON c.customer_id = o.customer_id 
-  JOIN order_items oi ON o.order_id = oi.order_id 
-GROUP BY 
-  c.email_address 
-ORDER BY 
-  item_price_total DESC;
+SELECT category_name, COUNT(*) AS product_count,
+MAX(list_price) AS MostExpensiveProduct
+FROM categories c JOIN products p
+ON c.category_id = p.category_id
+GROUP BY category_name
+ORDER BY product_ount DESC
