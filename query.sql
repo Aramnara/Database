ab62b6fc69a2c3c8cfe3c30ec2b1f3c6aa59fe92
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 16:43:17 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,17 +1,3 @@
-SELECT 
-    c.last_name, 
-    c.first_name, 
-    o.order_date, 
-    p.product_name, 
-    oi.list_price AS item_price, 
-    ROUND(oi.list_price * oi.discount_percent / 100, 2) AS discount_amount, 
-    oi.quantity 
-FROM 
-    customers c 
-    JOIN orders o ON c.customer_id = o.customer_id 
-    JOIN order_items oi ON o.order_id = oi.order_id 
-    JOIN products p ON oi.product_id = p.product_id 
-ORDER BY 
-    c.last_name, 
-    o.order_date, 
-    p.product_name;
+UPDATE categories
+SET category_name = 'Woodwinds'
+WHERE category_id = 1
