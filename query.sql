e6c6047e0fa349fc93d2db8c10f9a588195c8db1
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 17:28:40 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,9 +1,12 @@
 SELECT 
   c.email_address, 
-  SUM(oi.list_price * oi.quantity) AS item_price_total 
+  SUM(oi.list_price * oi.quantity) AS item_price_total, 
+  SUM((oi.list_price * oi.discount_percent / 100) * oi.quantity) AS discount_amount_total 
 FROM 
   customers c 
-  JOIN orders o ON c.customer_id = o.customer_id 
+  JOIN Orders o ON c.customer_id = o.customer_id 
   JOIN order_items oi ON o.order_id = oi.order_id 
 GROUP BY 
-  c.email_address;
+  c.email_address 
+ORDER BY 
+  item_price_total DESC;
