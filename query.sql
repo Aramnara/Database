679e2a807172038396326004a99101f7021b65e7
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 14:13:27 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -4,33 +4,35 @@ DELIMITER //
 
 CREATE PROCEDURE test()
 BEGIN
-DECLARE product_name_var VARCHAR(50);
-DECLARE list_price_var DECIMAL(9,2);
-DECLARE row_not_found TINYINT DEFAULT FALSE;
-DECLARE s_var VARCHAR(400) DEFAULT '';
+DECLARE product_name_var	VARCHAR(50);
+DECLARE list_price_var			DECIMAL(9,2);
+DECLARE row_not_found				TINYINT DEFAULT FALSE;
+DECLARE s_var								VARCHAR(400) DEFAULT '';
 
-DECLARE invoice_cursor CURSOR for
+DECLARE product_cursor CURSOR for
 	SELECT 
-		product_name,
+	       product_name,
 		list_price
-	FROM
-		products
-	WHERE
-		list_price > 700
-	ORDER BY list_price DESC;
+		FROM
+			products
+			WHERE
+				list_price > 700
+				ORDER BY list_price DESC;
     
 DECLARE CONTINUE HANDLER FOR NOT FOUND
 	SET row_not_found = TRUE;
 
-OPEN invoice_cursor;
+OPEN product_cursor;
 
-FETCH invoice_cursor INTO product_name_var, list_price_var;
+FETCH product_cursor INTO product_name_var, list_price_var;
 WHILE row_not_found = FALSE DO
-    SET s_var = CONCAT(s_var,' " ', product_name_var,' " , " ',list_price_var,'" |');
-	FETCH invoice_cursor INTO product_name_var, list_price_var;
+    SET s_var = CONCAT(s_var,'"',product_name_var,'","',list_price_var,'"|');
+    FETCH product_cursor INTO product_name_var,list_price_var;
 END WHILE;
 
 SELECT s_var AS message;
 END//
 
-call test();
+DELIMITER ;
+
+CALL test();
