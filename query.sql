ca2293a62350ca815f087a86fb1463c3d7705f34
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 18:11:03 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,12 +1,12 @@
 SELECT 
-    c.email_address AS email_address, 
-    SUM(oi.item_price * oi.quantity) AS item_price_total,
-    SUM(oi.discount_amount * oi.quantity) AS discount_amount_total
+    c.email_address, 
+    COUNT(o.order_id) AS order_count, 
+    SUM((oi.item_price - oi.discount_amount) * oi.quantity) AS order_total
 FROM 
     customers c 
     JOIN orders o ON c.customer_id = o.customer_id 
     JOIN order_items oi ON o.order_id = oi.order_id 
 GROUP BY 
     c.email_address 
-ORDER BY 
-    item_price_total DESC;
+HAVING 
+    COUNT(o.order_id) > 1;
