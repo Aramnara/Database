ccbda3320ebed7709b72dcd5e64cad6521525133
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 18:29:24 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,14 +1,14 @@
 SELECT 
-    c.email_address, 
-    COALESCE(COUNT(o.order_id), 0) AS order_count,
-    COALESCE(SUM(oi.item_price * oi.quantity), 0) AS order_total
-FROM 
-    customers c 
-    LEFT JOIN orders o ON c.customer_id = o.customer_id 
-    LEFT JOIN order_items oi ON o.order_id = oi.order_id 
-GROUP BY 
-    c.email_address 
-HAVING 
-    COUNT(o.order_id) > 1 
-ORDER BY 
-    order_total DESC;
+    c.email_address,
+    COUNT(o.order_id) AS count,
+    SUM(o.item_price - o.discount_amount) * COUNT(o.order_id) AS total_amount
+FROM
+    customers AS c
+        INNER JOIN
+    orders AS ord ON c.customer_id = ord.customer_id
+        INNER JOIN
+    order_items AS o ON o.order_id = ord.order_id 
+where o.item_price>400
+GROUP BY c.customer_id
+HAVING count > 1
+ORDER BY total_amount DESC;
