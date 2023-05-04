bb3431b085896a3dba4c53b3b46fc44b30213e72
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 18:44:21 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,10 +1,9 @@
-SELECT email_address, MAX(order_total) AS max_order_total, MIN(order_id) AS min_order_id
-FROM (
-  SELECT c.email_address, oi.order_id, SUM((oi.item_price - oi.discount_amount) * oi.quantity) AS order_total
-  FROM customers c
-  JOIN orders o ON c.customer_id = o.customer_id
-  JOIN order_items oi ON o.order_id = oi.order_id
-  GROUP BY c.email_address, oi.order_id
-) AS subquery
-GROUP BY email_address
-ORDER BY max_order_total DESC;
+SELECT product_name, list_price, discount_percent 
+FROM products 
+WHERE discount_percent IN (
+    SELECT discount_percent 
+    FROM products 
+    GROUP BY discount_percent 
+    HAVING COUNT(*) = 1
+) 
+ORDER BY product_name;
