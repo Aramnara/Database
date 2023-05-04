a4aa4629de0ddc4b6d9cc5f977c8c05e67fe2ded
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 15:19:59 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,22 +1,6 @@
-use my_guitar_shop;
-CREATE VIEW order_item_products AS
-SELECT
-    o.order_id,
-    o.order_date,
-    o.tax_amount,
-    o.ship_date,
-    oi.order_item_id,
-    oi.order_id AS oi_order_id,
-    oi.product_id,
-    oi.quantity,
-    oi.list_price,
-    p.product_id AS p_product_id,
-    p.product_name,
-    p.description,
-    p.category_id,
-    p.image_file_name,
-    p.active
-FROM
-    orders o
-    JOIN order_items oi ON o.order_id = oi.order_id
-    JOIN products p ON oi.product_id = p.product_id;
+SELECT product_name, list_price, discount_percent,
+ROUND(list_price*discount_percent/100, 2) AS 'discount_amount',
+(list_price - ROUND(list_price*discount_percent/100, 2)) AS 'discount_price'
+FROM products
+ORDER BY discount_price DESC
+LIMIT 5;
