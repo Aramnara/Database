c1c3e43a70618505b735e142bfe7d2fda7298df4
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 18:20:34 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,13 +1,12 @@
 SELECT 
     c.email_address,
-    COUNT(o.order_id) AS count,
-    SUM(o.item_price - o.discount_amount) * COUNT(o.order_id) AS total_amount
-FROM
-    customers AS c
-        INNER JOIN
-    orders AS ord ON c.customer_id = ord.customer_id
-        INNER JOIN
-    order_items AS o ON o.order_id = ord.order_id
-GROUP BY c.customer_id
-HAVING count > 1
-ORDER BY total_amount DESC;
+    COUNT(o.order_id) AS order_count,
+    SUM(oi.item_price * oi.quantity) AS order_total
+FROM 
+    customers c
+    JOIN orders o ON c.customer_id = o.customer_id
+    JOIN order_items oi ON o.order_id = oi.order_id
+GROUP BY 
+    c.email_address
+HAVING 
+    COUNT(o.order_id) > 1;
