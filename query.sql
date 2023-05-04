b931cf389447f4fe3cfbc20ed740aa2e7ab9e78a
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 15:13:32 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,15 +1,22 @@
 use my_guitar_shop;
-CREATE order_item_products AS
-    SELECT 
-        o.order_id,
-        o.order_date,
-        o.tax_amount,
-        o.ship_date,
-        o.item_price,
-        o.discount_amount,
-    FROM
-        orders o
-            JOIN
-        order_items oi ON o.order_id = oi.order_id
-            JOIN
-        products p ON oi.product_id = p.product_id;
+CREATE VIEW order_item_products AS
+SELECT
+    o.order_id,
+    o.order_date,
+    o.tax_amount,
+    o.ship_date,
+    oi.order_item_id,
+    oi.order_id AS oi_order_id,
+    oi.product_id,
+    oi.quantity,
+    oi.list_price,
+    p.product_id AS p_product_id,
+    p.product_name,
+    p.description,
+    p.category_id,
+    p.image_file_name,
+    p.active
+FROM
+    orders o
+    JOIN order_items oi ON o.order_id = oi.order_id
+    JOIN products p ON oi.product_id = p.product_id;
