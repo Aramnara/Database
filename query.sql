42c902a47b51d77558054b2769af4dbe7c156b85
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 17:01:00 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,11 @@
 SELECT 
-  COUNT(*) AS order_count, 
-  SUM(tax_amount) AS tax_total, 
-  AVG(tax_amount) AS tax_average 
+  c.category_name, 
+  COUNT(p.product_id) AS product_count, 
+  MAX(p.list_price) AS most_expensive_product 
 FROM 
-  orders;
+  Categories c 
+  JOIN products p ON c.category_id = p.category_id 
+GROUP BY 
+  c.category_name 
+ORDER BY 
+  COUNT(p.product_id) DESC;
