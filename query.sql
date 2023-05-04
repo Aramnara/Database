224f5f586f5ca9c68b7ff84414945fed15f2d194
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 19:20:12 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,8 +1,8 @@
 SELECT 
     list_price,
     FORMAT(list_price, 1) AS price_format,
-    CONVERT(list_price, INTEGER) AS price_convert,
-    CAST(list_price AS INTEGER) AS price_cast
+    CONVERT(list_price, SIGNED) AS price_convert,
+    CAST(list_price AS SIGNED) AS price_cast
 FROM 
     Products
 ORDER BY 
