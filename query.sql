844f0c14dd362173493637cdda42806eeec45624
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 17:22:20 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -2,8 +2,8 @@ SELECT
   c.email_address, 
   SUM(oi.list_price * oi.quantity) AS item_price_total 
 FROM 
-  Customers c 
-  JOIN Orders o ON c.customer_id = o.customer_id 
-  JOIN Order_Items oi ON o.order_id = oi.order_id 
+  customers c 
+  JOIN orders o ON c.customer_id = o.customer_id 
+  JOIN order_items oi ON o.order_id = oi.order_id 
 GROUP BY 
   c.email_address;
