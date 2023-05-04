b31f3bed45b584965f9abb8f6e3f3c7a57726ef8
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 18:35:19 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,16 @@
-SELECT product_id, product_name, list_price
-FROM products
-WHERE list_price > (SELECT AVG(list_price) FROM products)
-ORDER BY list_price DESC;
+SELECT c.email_address, oi.order_id, SUM((oi.item_price - oi.discount_amount) * oi.quantity) AS order_total
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+GROUP BY c.email_address, oi.order_id;
+
+SELECT email_address, MAX(order_total) AS max_order_total, MIN(order_id) AS min_order_id
+FROM (
+  SELECT c.email_address, oi.order_id, SUM((oi.item_price - oi.discount_amount) * oi.quantity) AS order_total
+  FROM customers c
+  JOIN orders o ON c.customer_id = o.customer_id
+  JOIN order_items oi ON o.order_id = oi.order_id
+  GROUP BY c.email_address, oi.order_id
+) AS subquery
+GROUP BY email_address
+ORDER BY max_order_total DESC;
