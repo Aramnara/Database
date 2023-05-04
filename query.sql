b9bfca2221ccbf001e884d9cabbb133bd993e33c
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 19:25:01 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,9 +1,9 @@
 SELECT 
-    list_price,
-    FORMAT(list_price, 1) AS price_format,
-    CONVERT(list_price, SIGNED) AS price_convert,
-    CAST(list_price AS SIGNED) AS price_cast
+    date_added, 
+    CAST(date_added AS DATE) AS added_date, 
+    CAST(date_added AS CHAR(7)) AS added_char7, 
+    CAST(date_added AS TIME) AS added_time 
 FROM 
-    products
+    Products 
 ORDER BY 
-    list_price ASC;
+    date_added ASC;
