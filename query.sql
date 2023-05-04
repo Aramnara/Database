9e501d78a384e2f425efda048912adc838884dba
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 19:40:33 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,11 +1,13 @@
 SELECT 
-    card_number,
-    LENGTH(card_number) AS card_number_length,
-    RIGHT(card_number, 4) AS last_four_digits,
-    IF(LENGTH(card_number) = 16,
-       CONCAT('XXXX-XXXX-XXXX-', RIGHT(card_number, 4)),
-       CONCAT('XXXX-XXXXXX-X', RIGHT(card_number, 4))) AS formatted_number
+    order_id, 
+    order_date, 
+    DATE_ADD(order_date, INTERVAL 2 DAY) AS approx_ship_date, 
+    ship_date, 
+    DATEDIFF(ship_date, order_date) AS days_to_ship
 FROM 
     orders
+WHERE 
+    order_date BETWEEN '2018-03-01' AND '2018-03-31' 
+    AND ship_date IS NOT NULL
 ORDER BY 
-    card_number ASC;
+    order_id ASC;
