af7ba1c5246dc18565ff455c43a64f52335d9615
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 16:19:53 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -8,9 +8,9 @@ SELECT
     oi.quantity 
 FROM 
     customers c 
-    JOIN Orders o ON c.customer_id = o.customer_id 
-    JOIN Order_Items oi ON o.order_id = oi.order_id 
-    JOIN Products p ON oi.product_id = p.product_id 
+    JOIN orders o ON c.customer_id = o.customer_id 
+    JOIN order_items oi ON o.order_id = oi.order_id 
+    JOIN products p ON oi.product_id = p.product_id 
 ORDER BY 
     c.last_name, 
     o.order_date, 
