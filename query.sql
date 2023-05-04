0172b7d01c064ba0a4b3efe26547ba5bca10d16d
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 16:10:09 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,17 +1,17 @@
 SELECT 
-  c.last_name, 
-  c.first_name, 
-  o.order_date, 
-  p.product_name, 
-  oi.list_price AS item_price, 
-  ROUND(oi.list_price * oi.discount_percent / 100, 2) AS discount_amount, 
-  oi.quantity 
+    c.last_name, 
+    c.first_name, 
+    o.order_date, 
+    p.product_name, 
+    oi.list_price AS item_price, 
+    ROUND(oi.list_price * oi.discount_percent / 100, 2) AS discount_amount, 
+    oi.quantity 
 FROM 
-  Customers c 
-  JOIN Orders o ON c.customer_id = o.customer_id 
-  JOIN Order_Items oi ON o.order_id = oi.order_id 
-  JOIN Products p ON oi.product_id = p.product_id 
+    Customers c 
+    JOIN Orders o ON c.customer_id = o.customer_id 
+    JOIN Order_Items oi ON o.order_id = oi.order_id 
+    JOIN Products p ON oi.product_id = p.product_id 
 ORDER BY 
-  c.last_name, 
-  o.order_date, 
-  p.product_name;
+    c.last_name, 
+    o.order_date, 
+    p.product_name;
