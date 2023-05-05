c10441fe9cfaf4df7971be6cda5c82fbad0b5039
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 20:12:12 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -3,7 +3,7 @@ SELECT p.product_name,
        COUNT(DISTINCT oi.order_id) AS order_count, 
        SUM(oi.quantity * oi.item_price * (1 - oi.discount_percent)) AS order_total
 FROM products p
-JOIN Order_Items oi ON p.product_id = oi.product_id
-JOIN Orders o ON oi.order_id = o.order_id
+JOIN order_items oi ON p.product_id = oi.product_id
+JOIN orders o ON oi.order_id = o.order_id
 GROUP BY p.product_id
 ORDER BY order_total DESC;
