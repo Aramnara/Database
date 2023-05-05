3bfad537363804360dc6aad45e2033f0d3e710ce
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 20:27:30 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,11 +1,18 @@
-CREATE PROCEDURE test
-AS
-BEGIN
-  DECLARE @product_count INT
-  SELECT @product_count = COUNT(*) FROM Products
+DROP PROCEDURE IF EXISTS test;
 
-  IF @product_count >= 7
-    SELECT 'The number of products is greater than or equal to 7' AS result
-  ELSE
-    SELECT 'The number of products is less than 7' AS result
-END
+DELIMITER //
+create procedure test()
+BEGIN
+DECLARE count_of_7 DECIMAL(10,2);
+
+select count(product_id) 
+into count_of_7
+from products;
+IF count_of_7 >= 7 THEN
+	SELECT 'The number of products is greater than or equal to 7' AS message;
+ELSE
+	SELECT 'The number of products is less than 7' AS message;
+end if;
+end//
+
+call test();
