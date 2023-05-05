00e30fd5326a654db37c60ddb2a175bbfec7c6fb
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-04 20:25:43 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,11 +1,11 @@
-CREATE VIEW order_item_products AS
-SELECT
-  products.product_name,
-  COUNT(DISTINCT orders.order_id) AS order_count,
-  SUM(order_items.quantity * order_items.unit_price) AS order_total
-FROM
-  products
-  INNER JOIN order_items ON products.product_id = order_items.product_id
-  INNER JOIN orders ON order_items.order_id = orders.order_id
-GROUP BY
-  products.product_id;
+CREATE PROCEDURE test
+AS
+BEGIN
+  DECLARE @product_count INT
+  SELECT @product_count = COUNT(*) FROM Products
+
+  IF @product_count >= 7
+    SELECT 'The number of products is greater than or equal to 7' AS result
+  ELSE
+    SELECT 'The number of products is less than 7' AS result
+END
