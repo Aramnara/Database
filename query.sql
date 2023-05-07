e24d300c411052591d427de66682d98a4f54d436
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 16:48:23 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,29 +1,5 @@
-CREATE PROCEDURE test(OUT msg VARCHAR(255))
-BEGIN
-    DECLARE product_name VARCHAR(255);
-    DECLARE list_price DECIMAL(10,2);
-    DECLARE result VARCHAR(255) DEFAULT '';
-    DECLARE done INT DEFAULT 0;
-
-    DECLARE cur CURSOR FOR
-    SELECT product_name, list_price
-    FROM products
-    WHERE list_price > 700
-    ORDER BY list_price DESC;
-
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
-
-    OPEN cur;
-
-    read_loop: LOOP
-        FETCH cur INTO product_name, list_price;
-        IF done THEN
-            LEAVE read_loop;
-        END IF;
-        SET result = CONCAT(result, '*', product_name, '*', ',','*', list_price, '*', '|');
-    END LOOP;
-
-    CLOSE cur;
-
-    SET msg = result;
-END;
+CREATE VIEW product_summary AS
+SELECT p.product_name, COUNT(DISTINCT oi.order_id) AS order_count, SUM(oi.item_total) AS order_total
+FROM products AS p
+JOIN order_items AS oi ON p.product_id = oi.product_id
+GROUP BY p.product_name;
