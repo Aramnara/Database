818296aa9e38a2abd4689ca99830955bf876cac8
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 16:08:26 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,10 +1,16 @@
 CREATE PROCEDURE test()
 BEGIN
-    DECLARE result VARCHAR(255);
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
-        SET result = 'Row was not inserted - duplicate entry.';
-    INSERT INTO Categories (category_name)
-    VALUES ('Guitars');
-    SET result = '1 row was inserted.';
-    SELECT result AS result;
+    DECLARE lresult VARCHAR(100);
+    
+    SET lresult = '';
+
+    INSERT INTO Categories (category_name) VALUES ('Guitars');
+    
+    IF ROW_COUNT() = 1 THEN
+        SET lresult = '1 row was inserted.';
+    ELSE
+        SET lresult = 'Row was not inserted - duplicate entry.';
+    END IF;
+    
+    SELECT lresult AS result;
 END;
