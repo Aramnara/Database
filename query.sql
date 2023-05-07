c5f4dadbfd4396faec2863a3c7855340dce0e552
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 15:57:33 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,10 +1,26 @@
 CREATE PROCEDURE test()
 BEGIN
-    DECLARE product_count INT DEFAULT 0;
-    SELECT COUNT(*) INTO product_count FROM products;
-    IF (product_count >= 7) THEN
-        SELECT 'The number of products is greater than or equal to 7' AS result;
-    ELSE
-        SELECT 'The number of products is less than 7' AS result;
-    END IF;
+    DECLARE product_name VARCHAR(255);
+    DECLARE list_price DECIMAL(10,2);
+    DECLARE result VARCHAR(5000) DEFAULT '';
+    
+    DECLARE product_cursor CURSOR FOR
+        SELECT product_name, list_price
+        FROM Products
+        WHERE list_price > 700
+        ORDER BY list_price DESC;
+        
+    OPEN product_cursor;
+    
+    FETCH product_cursor INTO product_name, list_price;
+    WHILE (FOUND_ROWS() > 0) DO
+        SET result = CONCAT(result, '*', product_name, '*', ',', '*', FORMAT(list_price, 2), '*', '|');
+        FETCH product_cursor INTO product_name, list_price;
+    END WHILE;
+    
+    CLOSE product_cursor;
+    
+    SELECT result AS result;
 END;
+
+CALL test();
