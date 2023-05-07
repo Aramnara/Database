f6b0da6e90ccd4a76ec766e7e4966e0929a63fdb
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 16:29:12 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,9 +1,8 @@
-
 CREATE FUNCTION discount_price(item_id INT)
 RETURNS DECIMAL(10,2)
 BEGIN
     DECLARE discount DECIMAL(10,2);
     DECLARE item_price DECIMAL(10,2);
-    SELECT discount_amount, item_price INTO discount, item_price FROM order_items WHERE order_item_id = item_id;
+    SELECT discount_amount, price INTO discount, item_price FROM order_items WHERE item_id = order_item_id;
     RETURN (item_price - discount);
 END;
