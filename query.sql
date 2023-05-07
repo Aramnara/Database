14945d799c05c6ba1a792b6978d965441794008a
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 16:45:11 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,20 +1,29 @@
-CREATE PROCEDURE test(OUT message VARCHAR(1000))
+CREATE PROCEDURE test(OUT msg VARCHAR(255))
 BEGIN
     DECLARE product_name VARCHAR(255);
     DECLARE list_price DECIMAL(10,2);
-    DECLARE result_string VARCHAR(1000) DEFAULT '';
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE product_cursor CURSOR FOR SELECT product_name, list_price FROM products WHERE list_price > 700 ORDER BY list_price DESC;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-    
-    OPEN product_cursor;
+    DECLARE result VARCHAR(255) DEFAULT '';
+    DECLARE done INT DEFAULT 0;
+
+    DECLARE cur CURSOR FOR
+    SELECT product_name, list_price
+    FROM Products
+    WHERE list_price > 700
+    ORDER BY list_price DESC;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    OPEN cur;
+
     read_loop: LOOP
-        FETCH product_cursor INTO product_name, list_price;
+        FETCH cur INTO product_name, list_price;
         IF done THEN
             LEAVE read_loop;
         END IF;
-        SET result_string = CONCAT(result_string, '*', product_name, '*', ',', '*', FORMAT(list_price, 2), '*', '|');
+        SET result = CONCAT(result, '*', product_name, '*', ',','*', list_price, '*', '|');
     END LOOP;
-    CLOSE product_cursor;
-    SET message = result_string;
+
+    CLOSE cur;
+
+    SET msg = result;
 END;
