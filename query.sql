d343ba4414f07397b8d6627ab027ebdae4c5ce5f
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 16:25:16 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,26 +1,8 @@
-DROP FUNCTION IF EXISTS discount_price;
-
-DELIMITER //
-
-CREATE FUNCTION discount_price
-(
-  item_id_param	INT(11)
-)
+CREATE FUNCTION discount_price(item_id INT)
 RETURNS DECIMAL(10,2)
 BEGIN
-  DECLARE discount_price_var DECIMAL(10,2);
-    
-  SELECT item_price - discount_amount
-  INTO   discount_price_var
-  FROM   order_items
-  WHERE  item_id = item_id_param;
-    
-  RETURN discount_price_var;
-END//
-
-DELIMITER ;
-
--- Check:
-SELECT item_id, item_price, discount_amount, 
-       discount_price(item_id) as discount_price
-FROM order_items;
+    DECLARE discount DECIMAL(10,2);
+    DECLARE item_price DECIMAL(10,2);
+    SELECT discount_amount, list_price INTO discount, item_price FROMoOrder_items WHERE order_item_id = item_id;
+    RETURN (item_price - discount);
+END;
