3e02f1242aab70c1523805db128639f8fff35707
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 15:46:27 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,19 +1,26 @@
+DROP PROCEDURE IF EXISTS test;
+
 DELIMITER //
 
 CREATE PROCEDURE test()
 BEGIN
-  DECLARE factors VARCHAR(255);
-  DECLARE i INT;
+  DECLARE i              INT DEFAULT 1;
+  DECLARE number_1_var   INT DEFAULT 10;
+  DECLARE number_2_var   INT DEFAULT 20;
+  DECLARE message_var    VARCHAR(400);
   
-  SET factors = 'Common factors of 10 and 20: ';
+  SET message_var = CONCAT('Common factors of ', number_1_var, ' and ',number_2_var,':');
+  WHILE ((i <= number_1_var) AND (i < number_2_var))  DO
+    
+    IF ((number_1_var % i = 0) AND (number_2_var % i = 0)) THEN
+      SET message_var = CONCAT(message_var," ", i);
+      END IF;
+    
+    SET i = i + 1;
+  END WHILE;
   
-  FOR i = 1 TO 10 DO
-    IF (10 % i = 0 AND 20 % i = 0) THEN
-      SET factors = CONCAT(factors, i, ' ');
-    END IF;
-  END FOR;
-  
-  SELECT factors AS result;
+  SELECT message_var AS message;
+
 END//
 
 DELIMITER ;
