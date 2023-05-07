9a98891b41c1c9ad0a7be5feb9c7a17ade4bb662
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 16:15:36 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,10 +1,5 @@
-CREATE PROCEDURE test(OUT message VARCHAR(255))
+CREATE PROCEDURE insert_category(IN category_name VARCHAR(255))
 BEGIN
-    DECLARE product_count INT DEFAULT 0;
-    SELECT COUNT(*) INTO product_count FROM products;
-    IF (product_count >= 7) THEN
-        SET message = 'The number of products is greater than or equal to 7';
-    ELSE
-        SET message = 'The number of products is less than 7';
-    END IF;
+    INSERT INTO categories (category_name)
+    VALUES (category_name);
 END;
