8c38427eafd4f8c374ec9c4448c935895595808a
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 15:44:10 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,12 +1,21 @@
+DELIMITER //
+
 CREATE PROCEDURE test()
 BEGIN
-    DECLARE factors VARCHAR(255) DEFAULT '';
-    DECLARE i INT DEFAULT 1;
-    WHILE (i <= 10) DO
-        IF (10 % i = 0 AND 20 % i = 0) THEN
-            SET factors = CONCAT(factors, i, ' ');
-        END IF;
-        SET i = i + 1;
-    END WHILE;
-    SELECT CONCAT('Common factors of 10 and 20: ', factors) AS result;
-END;
+  DECLARE factors VARCHAR(255);
+  DECLARE i INT;
+  
+  SET factors = 'Common factors of 10 and 20: ';
+  
+  FOR i = 1 TO 10 DO
+    IF (10 % i = 0 AND 20 % i = 0) THEN
+      SET factors = CONCAT(factors, i, ' ');
+    END IF;
+  END FOR;
+  
+  SELECT factors AS result;
+END//
+
+DELIMITER ;
+
+CALL test();
