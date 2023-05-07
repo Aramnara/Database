071d14e26025522f89bb131965cb47fe43c6be59
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 15:54:09 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE test()
 BEGIN
     DECLARE product_count INT DEFAULT 0;
-    SELECT COUNT(*) INTO product_count FROM Products;
+    SELECT COUNT(*) INTO product_count FROM products;
     IF (product_count >= 7) THEN
         SELECT 'The number of products is greater than or equal to 7' AS result;
     ELSE
