2e0f2707231dfa232415c767d3d887a604708979
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 16:35:50 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -3,6 +3,6 @@ RETURNS DECIMAL(10,2)
 BEGIN
     DECLARE discount DECIMAL(10,2);
     DECLARE item_price DECIMAL(10,2);
-    SELECT discount_amount, price INTO discount, item_price FROM order_items WHERE order_item_id = item_id;
+    SELECT discount_amount, item_price INTO discount, item_price FROM order_items WHERE order_item_id = item_id;
     RETURN (item_price - discount);
 END;
