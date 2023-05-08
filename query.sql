970603533f39eafa9fbe97f380211f7606b93cdf
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 22:19:28 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
-SELECT c.last_name, c.first_name, o.order_date, p.product_name, oi.item_price, oi.discount_amount, oi.quantity
-FROM customers AS c
-JOIN orders AS o ON c.customer_id = o.customer_id
-JOIN order_items AS oi ON o.order_id = oi.order_id
-JOIN products AS p ON oi.product_id = p.product_id
+SELECT c.last_name, COALESCE(c.first_name, 'N/A') AS first_name, o.order_date, p.product_name, oi.item_price, oi.discount_amount, oi.quantity
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id
 ORDER BY c.last_name, o.order_date, p.product_name;
