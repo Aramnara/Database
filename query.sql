aa5cbe25e0963646d1534b6825a92aed3e68d651
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 21:56:39 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,13 +1,6 @@
-SELECT 
-    order_id,
-    order_date,
-    DATE_ADD(order_date, INTERVAL 2 DAY) AS approx_ship_date,
-    ship_date,
-    DATEDIFF(ship_date, order_date) AS days_to_ship
-FROM 
-    orders
-WHERE 
-    order_date BETWEEN '2018-03-01' AND '2018-03-31' 
-    AND ship_date IS NOT NULL
-ORDER BY 
-    order_id ASC;
+SELECT order_id, order_date, DATE_ADD(order_date, INTERVAL 2 DAY) AS approx_ship_date, 
+       COALESCE(ship_date, DATE_ADD(order_date, INTERVAL 2 DAY)) AS ship_date, 
+       DATEDIFF(COALESCE(ship_date, DATE_ADD(order_date, INTERVAL 2 DAY)), order_date) AS days_to_ship
+FROM orders
+WHERE YEAR(order_date) = 2018 AND MONTH(order_date) = 3
+ORDER BY order_id ASC;
