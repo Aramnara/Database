6da87d801c3b5ab5dd96b2ec58fa446039e7da24
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 22:12:27 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,15 +1,8 @@
-SELECT c.email_address, 
-       COUNT(DISTINCT o.order_id) AS order_count, 
-       SUM((oi.item_price - oi.discount_amount) * oi.quantity) AS order_total
-FROM customers AS c
-JOIN orders AS o ON c.customer_id = o.customer_id
-JOIN order_items AS oi ON o.order_id = oi.order_id
-WHERE c.customer_id IN (
-  SELECT customer_id
-  FROM orders
-  GROUP BY customer_id
-  HAVING COUNT(*) > 1
-)
-AND oi.item_price > 400
+SELECT c.email_address, COUNT(DISTINCT o.order_id) AS order_count, 
+SUM((oi.item_price - oi.discount_amount) * oi.quantity) AS order_total
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id AND oi.item_price > 400
 GROUP BY c.email_address
+HAVING COUNT(DISTINCT o.order_id) > 1
 ORDER BY order_total DESC;
