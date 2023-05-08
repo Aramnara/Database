d22c9e94501c1dbb2562261bbefe52f09d1bb178
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 21:51:14 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,6 @@
 CREATE VIEW product_summary AS
-SELECT p.product_name, COUNT(DISTINCT oi.order_id) AS order_count, SUM(oi.item_total) AS order_total
+SELECT p.product_id, p.product_name, COUNT(DISTINCT oi.order_id) AS order_count,
+SUM(oi.item_total) AS order_total
 FROM products AS p
-JOIN order_items AS oi ON p.product_id = oi.product_id
-GROUP BY p.product_name;
+JOIN order_items AS oi ON oi.product_id = p.product_id
+GROUP BY p.product_id, p.product_name;
