e17e7677183ad6e916962a02d1a3a729895d78bc
Anthony Ramnarain <anthonyramnarain7@gmail.com>
2023-05-07 21:53:35 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
-CREATE VIEW product_summary AS
-SELECT p.product_id, p.product_name, COUNT(DISTINCT oi.order_id) AS order_count,
-SUM(oi.item_total) AS order_total
-FROM products AS p
-JOIN order_items AS oi ON oi.product_id = p.product_id
-GROUP BY p.product_id, p.product_name;
+SELECT card_number,
+       LENGTH(card_number) AS card_number_length,
+       RIGHT(card_number, 4) AS last_four_digits,
+       CONCAT(IF(LENGTH(card_number) = 16, CONCAT(REPEAT('X', 12), '-', RIGHT(card_number, 4)), CONCAT(REPEAT('X', 10), '-', RIGHT(card_number, 4)))), AS formatted_number
+FROM orders
+ORDER BY card_number ASC;
